e7c7c197b6a7b32630cb08cc5499bb7971541894
Chloe Zhu <chloezhu@Chloes-MacBook-Pro.local>
2026-02-05 03:00:32 -0800

    Add new folder: apps

---
+++ apps/apps/switch_status_inspection_image_proc+OCR_Prototype/cmake-build-debug/CMakeFiles/switch_status_inspection_image_processing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SwitchDetectionNew.dir/main.cpp.o"
+  "SwitchDetectionNew.pdb"
+  "SwitchDetectionNew"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SwitchDetectionNew.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ apps/apps/text_detection_east/cmake-build-debug/CMakeFiles/TextDetection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TextDetection.dir/main.cpp.o"
+  "TextDetection.pdb"
+  "TextDetection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TextDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
