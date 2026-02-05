a6b852a7ec6aac9041358467ee2f244b1620f544
Chloe Zhu <chloezhu@Chloes-MacBook-Pro.local>
2026-02-05 02:46:11 -0800

    Add new folder: apps

---
+++ apps/text_recognition_tesseract/cmake-build-debug/CMakeFiles/TextRec.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TextRec.dir/main.cpp.o"
+  "TextRec.pdb"
+  "TextRec"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TextRec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
